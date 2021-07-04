8468981b11b41ac0a894766cd16a218f05a4d7a1
Ziva <birulangit1996@gmail.com>
2021-07-04 10:10:48 +0700

    add chart

---
+++ src/pages/home/Header.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Header = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get('https://covid19.mathdro.id/api').then((data) => {
+        setData(data.data);
+      });
+    } catch (error) {
+      console.log(error.response);
+    }
+  }, []);
+
+  return (
+    <div className="bg-gray-200">
+      <div
+        className="bg-green-400 lg:rounded-b-full sm:rounded-b-xl pt-4 px-10"
+        style={{ height: '200px' }}
+      >
+        <h1 className="text-center font-bold text-white mb-4">
+          Corona Tracker
+        </h1>
+        {data ? (
+          <div className="grid grid-cols-3 text-center text-white">
+            <div className="shadow rounded-lg mx-3 bg-blue-400 py-6">
+              <h1>Confirmed</h1>
+              <h2 className="">{data.confirmed?.value}</h2>
+            </div>
+            <div className="shadow rounded-lg mx-3 bg-yellow-400 py-6">
+              <h1>Recovered</h1>
+              <h2>{data.recovered?.value}</h2>
+            </div>
+            <div className="shadow rounded-lg mx-3 bg-red-400 py-6">
+              <h1>Deaths</h1>
+              <h2>{data.deaths?.value}</h2>
+            </div>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
